869bae02659ccc647e0d8411aa15f7d52c2e23cd
Ajit Shatvapa Gadkari <ajit@Ajits-MacBook-Pro.local>
2023-12-06 15:03:26 +0530

    local env

--- server/index.js
+++ server/index.js
@@ -6,20 +6,18 @@ const Users =require("./models/userModel");
 const PORT = process.env.PORT||3333
 
 app.use(express.json());
-app.use(cors({
-    origin: 'https://shiv-frontend.vercel.app/',
-    credentials: true,
-  }));
+app.use(cors());
 const {dbConnect}=require("./mongoDb/dbConnection");
 dbConnect(process.env.MONGO_URL);
 const router=require("./routes/userRoutes");
 const postRouter=require("./routes/postRoutes");
-
 const paymentRouter=require("./routes/paymentRoute")
+const aboutRouter=require("./routes/aboutRoutes")
 app.use("/api/v1/auth",postRouter);
 app.use("/api/auth",router);
 
 app.use("",paymentRouter);
+app.use("",aboutRouter);
 app.use("/",(req,resp)=>{
     
     resp.send("server running");
--- server/routes/paymentRoute.js
+++ server/routes/paymentRoute.js
@@ -1,13 +1,12 @@
-const { createPayment, razor_key, paymentVerification } = require('../controllers/paymentController');
+const { createPayment, razor_key, paymentVerification,getPaymentDetails } = require('../controllers/paymentController');
 const express=require("express");
 const paymentRouter=express.Router();
-
 
 
 paymentRouter.post("/api/payment",createPayment);
 paymentRouter.post("/api/paymentVerification",paymentVerification);
 paymentRouter.get("/api/key",razor_key);
-
+paymentRouter.get("/api/paymentDetails",getPaymentDetails);
 
 
 module.exports= paymentRouter;
